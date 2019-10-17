1c94a383d8385c7cf0c5c495d5fb728e76ec6bb0
= <=>
2019-10-17 00:31:58 -0300

    new libraries added

--- app/src/main/java/com/rphmelo/myexchange/rates/ui/RatesActivity.kt
+++ app/src/main/java/com/rphmelo/myexchange/rates/ui/RatesActivity.kt
@@ -1,4 +1,4 @@
-package com.rphmelo.myexchange.rates
+package com.rphmelo.myexchange.rates.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
---
+++ app/src/main/java/com/rphmelo/myexchange/rates/ui/RatesListAdapter.kt
@@ -0,0 +1,45 @@
+package com.rphmelo.myexchange.rates.ui
+
+import android.content.Context
+import android.support.design.widget.TextInputEditText
+import android.support.design.widget.TextInputLayout
+import android.support.v7.widget.RecyclerView
+import android.text.Editable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.rphmelo.myexchange.R
+
+class RatesListAdapter (
+    private val context: Context,
+    private var ratesList: HashMap<String, Double>
+): RecyclerView.Adapter<RatesListAdapter.RatesListViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RatesListViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.view_item_list_rates, parent, false)
+        return RatesListViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return ratesList.toList().size
+    }
+
+    override fun onBindViewHolder(holder: RatesListViewHolder, position: Int) {
+        holder.bindView(ratesList.toList()[position])
+    }
+
+    inner class RatesListViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+        private val txtDescription: TextView = itemView.findViewById(R.id.textview_view_item_list_rates_description)
+        private val txtCode: TextView = itemView.findViewById(R.id.textview_view_item_list_rates_code)
+        private var inputLayoutItemListRates: TextInputLayout = itemView.findViewById(R.id.input_layout_item_list_rates)
+        private var inputEditTextItemListRates: TextInputEditText = itemView.findViewById(R.id.input_edit_text_item_list_rates)
+
+        fun bindView(rate: Pair<String, Double>) = with(itemView){
+            txtCode.text = rate.first
+            inputEditTextItemListRates.text = rate.second as Editable
+        }
+
+    }
+}
