d5b2b5f87a242dd476ee8946be2a99f6255e5c2d
= <=>
2019-10-22 22:47:00 -0300

    add round to rate values

--- app/src/main/java/com/rphmelo/myexchange/extension/DoubleExt.kt
+++ app/src/main/java/com/rphmelo/myexchange/extension/DoubleExt.kt
@@ -1,6 +1,12 @@
 package com.rphmelo.myexchange.extension
 
+import java.math.RoundingMode
+
 fun Double.convertCurrency(rateOldValue: Double, rateNewValue: Double): Double {
     val ratio = this / rateOldValue
-    return ratio * rateNewValue
+    return (ratio * rateNewValue).round(4)
+}
+
+fun Double.round(decimals: Int): Double {
+    return this.toBigDecimal().setScale(decimals, RoundingMode.UP).toDouble()
 }
