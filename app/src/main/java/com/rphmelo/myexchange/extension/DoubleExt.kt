db13069ad07373224c3e3fc1afcb3c750eb26a45
= <=>
2019-10-23 01:17:09 -0300

    add validation for not calculating zero values

--- app/src/main/java/com/rphmelo/myexchange/extension/DoubleExt.kt
+++ app/src/main/java/com/rphmelo/myexchange/extension/DoubleExt.kt
@@ -3,7 +3,11 @@ package com.rphmelo.myexchange.extension
 import java.math.RoundingMode
 
 fun Double.convertCurrency(rateOldValue: Double, rateNewValue: Double): Double {
-    val ratio = this / rateOldValue
+    var ratio = 0.0
+
+    if(this > 0 && rateOldValue > 0){
+        ratio = this / rateOldValue
+    }
     return (ratio * rateNewValue).round(4)
 }
 
